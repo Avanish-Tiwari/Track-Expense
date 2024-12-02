4f118e27221ae5becc8e76c3b6c5c47bdba67845
Avanish Tiwari <avanishtiwari@outlook.in>
2024-12-02 23:06:59 +0530

    contextMenu implementation

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react"
 
-export default function ContextMenu() {
+export default function ContextMenu({ menuPosition, setMenuPosition, setExpenses, rowID }) {
+  if (!menuPosition.left) return
   return (
-    <div className="context-menu">
-          <div>Edit</div>
-          <div>Delete</div>
+    <div className="context-menu" style={menuPosition}>
+      <div
+        onClick={() => {
+          console.log("editing")
+          setMenuPosition({})
+        }}
+      >
+        Edit
       </div>
+      <div
+        onClick={() => {
+          setExpenses((prevState)=>prevState.filter((expense)=>expense.id!=rowID))
+          setMenuPosition({})
+        }}
+      >
+        Delete
+      </div>
+    </div>
   )
 }
